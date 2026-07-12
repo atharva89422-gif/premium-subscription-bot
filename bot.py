41a1e8c6c62e644a4e479c0ca26c70d178e8b137
atharva89422-gif <atharva89422@gmail.com>
2026-07-12 12:06:00 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,12 @@
 import telebot
 
 from config import BOT_TOKEN
-from handlers import init_bot, show_home
+from handlers import (
+    init_bot,
+    show_home,
+    show_plans,
+    show_payment
+)
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
@@ -11,6 +16,27 @@ init_bot(bot)
 @bot.message_handler(commands=["start"])
 def start(message):
     show_home(message)
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callbacks(call):
+
+    if call.data == "premium":
+        show_plans(call.message.chat.id)
+
+    elif call.data == "plan_7_days":
+        show_payment(call.message.chat.id, "7_days")
+
+    elif call.data == "plan_15_days":
+        show_payment(call.message.chat.id, "15_days")
+
+    elif call.data == "plan_30_days":
+        show_payment(call.message.chat.id, "30_days")
+
+    elif call.data == "plan_1_year":
+        show_payment(call.message.chat.id, "1_year")
+
+    bot.answer_callback_query(call.id)
 
 
 print("Bot Started...")
