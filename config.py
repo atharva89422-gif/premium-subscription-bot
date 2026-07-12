cb53a6d80bd3ebfee107ec655b170e68a4d3101a
atharva89422-gif <atharva89422@gmail.com>
2026-07-12 11:55:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -18,12 +18,25 @@ CHANNEL_NAME = "Premium Channel"
 PLANS = {
     "7_days": {
         "name": "7 Days",
-        "price": 99,
+        "price": 69,
         "days": 7
     },
+
+    "15_days": {
+        "name": "15 Days",
+        "price": 110,
+        "days": 15
+    },
+
     "30_days": {
         "name": "30 Days",
         "price": 199,
         "days": 30
+    },
+
+    "1_year": {
+        "name": "1 Year",
+        "price": 499,
+        "days": 365
     }
 }
